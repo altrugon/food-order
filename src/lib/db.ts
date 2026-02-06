3eebac09348e8f3121f6010f2458567703d70222
Alberto Trujillo Gonzalez <altrugon@gmail.com>
2026-02-06 04:36:49 -0800

    Add more menu items.

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -4,14 +4,35 @@ export const menu = [
     name: "Pizza",
     description: "Cheese pizza",
     price: 12,
-    image: "/images/pizza.svg",
+    image: "/menu-images/pizza.svg",
   },
   {
     id: "2",
     name: "Burger",
     description: "Beef burger",
     price: 10,
-    image: "/images/burger.svg",
+    image: "/menu-images/burger.svg",
+  },
+  {
+    id: "3",
+    name: "Beer",
+    description: "Local Beer",
+    price: 8,
+    image: "/menu-images/beer.svg",
+  },
+  {
+    id: "4",
+    name: "Coffee",
+    description: "Hot Coffee",
+    price: 5,
+    image: "/menu-images/coffee.svg",
+  },
+  {
+    id: "5",
+    name: "Pop",
+    description: "Cold Pop",
+    price: 3,
+    image: "/menu-images/pop.svg",
   },
 ];
 
